33747638de1d7edf0b68775e2d86296e1289fbbb
Christian Smith <smith.christian.william@gmail.com>
2024-09-26 18:57:16 -0400

    containers

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("vdat")
+	l := widget.NewLabel("Hello World!")
+	l.Alignment = fyne.TextAlignCenter
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	b := container.NewBorder(nil, nil, nil, nil, l)
+
+	w.SetContent(b)
 	w.ShowAndRun()
 }
