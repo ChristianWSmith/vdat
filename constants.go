39a6e484cbf15d76116c5b780d3f4fefe4b7d3db
Christian Smith <smith.christian.william@gmail.com>
2024-09-30 14:23:04 -0400

    space rune is valid

--- constants.go
+++ constants.go
@@ -23,7 +23,7 @@ var VALID_RUNES = []rune{
 	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-	'-', '_', '.', '~', '!', '#', '$', '&', '(', ')', '*', '+', ',', '/', ':', ';', '=', '?', '@', '[', ']'}
+	' ', '-', '_', '.', '~', '!', '#', '$', '&', '(', ')', '*', '+', ',', '/', ':', ';', '=', '?', '@', '[', ']'}
 
 const HEADERS_PLACEHOLDER = "# comment\nheader1 <tab> value1\nheader2 <tab> value2"
 const PARAMS_PLACEHOLDER = "# comment\nparam1=value1\nparam2=value2"
